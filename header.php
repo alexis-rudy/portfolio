c94a7007690bc3ad07e63e1e70beabff8521e558
alexis <91147834+alexis-rudy@users.noreply.github.com>
2023-01-25 17:21:37 -0700

    fixed header

---
+++ header.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="index.html"><img src="img/home.png"></a></li>
+        <li><a href="#"><img src="img/clip.png"></a></li>
+        <li><a href="contactme.html"><img src="img/envelope.png"></a></li>
+        <li><a href="aboutme.html"><img src="img/user.png"></a></li>
+    </ul>
+</nav>
